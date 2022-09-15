62c65e2b586599597d52c919ce440264968eaa73
Sadrach Martinez <63802470+sadrachm@users.noreply.github.com>
2022-09-15 16:40:47 -0700

    Discarded unnecessary changes

--- src/pages/GameShrine/Admin/admin.jsx
+++ src/pages/GameShrine/Admin/admin.jsx
@@ -4,18 +4,14 @@ import { Button } from "react-bootstrap";
 import Form from "react-bootstrap/Form";
 import PostCreation from "./postCreation";
 import PostViewer from "./postViewer";
-import PostEditor from "./postEditor";
 const Admin = () => {
   const [view, setView] = useState(false)
-  function handleClick() {
-    setView((view+1)%3)
-  }
+  
   return (
     <div style={{color:"black"}}>
-      <Button onClick={handleClick}>Switch</Button>
-      {view==0 && <PostCreation></PostCreation>}
-      {view==1 && <PostViewer></PostViewer>}
-      {view==2 && <PostEditor></PostEditor>}
+      <Button onClick={()=>{setView(!view)}}>Switch</Button>
+      {view && <PostCreation></PostCreation>}
+      {!view && <PostViewer></PostViewer>}
     </div>
   );
 };
