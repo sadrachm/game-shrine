9c93e94af5935e741a2c8c61041dc55d986438f6
Sadrach Martinez <63802470+sadrachm@users.noreply.github.com>
2022-12-12 16:07:50 -0800

    Taskbar fades out on scroll down for Lista

--- src/pages/Lista/lista.jsx
+++ src/pages/Lista/lista.jsx
@@ -1,6 +1,10 @@
 import { useEffect, useState } from "react";
 import "./lista.css";
-import { createProductos, deleteProductos, updateList } from "../../graphql/mutations";
+import {
+  createProductos,
+  deleteProductos,
+  updateList,
+} from "../../graphql/mutations";
 import { listLists, listProductos } from "../../graphql/queries";
 import { API } from "aws-amplify";
 import Card from "./Components/Card";
@@ -8,6 +12,7 @@ import SendIcon from "@mui/icons-material/Send";
 import ListTest from "./Components/listTest";
 import { Grid, TextField } from "@mui/material";
 import { Button } from "react-bootstrap";
+import { motion, AnimatePresence } from "framer-motion";
 
 var stores = {};
 var productIds = {};
@@ -17,9 +22,12 @@ let order = {};
 let maxima = 0;
 
 const Lista = () => {
+  const threshold = 50;
+  const [scrollDir, setScrollDir] = useState("up");
   const [store, setStore] = useState(0);
   const [entry, setEntry] = useState("");
   const [items, setItems] = useState([]);
+  const [y, setY] = useState(window.scrollY);
 
   async function start() {
     let y = await API.graphql({ query: listLists });
@@ -33,7 +41,39 @@ const Lista = () => {
 
   useEffect(() => {
     start();
+    // Credit: https://www.fabrizioduroni.it/2022/01/02/react-detect-scroll-direction/
+    let previousScrollYPosition = window.scrollY;
+
+    const scrolledMoreThanThreshold = (currentScrollYPosition) =>
+      Math.abs(currentScrollYPosition - previousScrollYPosition) > threshold;
+
+    const isScrollingUp = (currentScrollYPosition) =>
+      currentScrollYPosition > previousScrollYPosition &&
+      !(previousScrollYPosition > 0 && currentScrollYPosition === 0) &&
+      !(currentScrollYPosition > 0 && previousScrollYPosition === 0);
+
+    const updateScrollDirection = () => {
+      const currentScrollYPosition = window.scrollY;
+
+      if (scrolledMoreThanThreshold(currentScrollYPosition)) {
+        const newScrollDirection = isScrollingUp(currentScrollYPosition)
+          ? "down"
+          : "up";
+        setScrollDir(newScrollDirection);
+        previousScrollYPosition =
+          currentScrollYPosition > 0 ? currentScrollYPosition : 0;
+      }
+    };
+
+    const onScroll = () => window.requestAnimationFrame(updateScrollDirection);
+
+    window.addEventListener("scroll", onScroll);
+
+    return () => window.removeEventListener("scroll", onScroll);
   }, []);
+  useEffect(() => {
+    console.log(scrollDir);
+  }, [scrollDir]);
 
   useEffect(() => {
     if (store === 0) {
@@ -54,7 +94,7 @@ const Lista = () => {
       variables: { filter: { productosListId: { eq: storeObject } } },
     });
     const products = apiData.data.listProductos.items;
-    console.log("PRODUCTS", products)
+    console.log("PRODUCTS", products);
     orderProduct = {};
     productOrder = {};
     productIds = {};
@@ -66,7 +106,7 @@ const Lista = () => {
       productOrder[el.order] = el.name;
       orderProduct[el.name] = el.order;
     });
-    console.log("PRODUCT_ORDER", productOrder)
+    console.log("PRODUCT_ORDER", productOrder);
     let x = [];
     for (var el of order[storeObject]) {
       if (productOrder[el] === undefined) {
@@ -74,8 +114,8 @@ const Lista = () => {
       }
       x.push(productOrder[el]);
     }
-    console.log("Items", x)
-    console.log("Orders", order)
+    console.log("Items", x);
+    console.log("Orders", order);
     setItems(x);
   }
 
@@ -111,7 +151,7 @@ const Lista = () => {
 
   async function updateOrder() {
     let newItemOrder = [];
-    console.log("Update List", items)
+    console.log("Update List", items);
     items.map((el) => {
       newItemOrder.push(orderProduct[el]);
     });
@@ -123,25 +163,24 @@ const Lista = () => {
     });
   }
   async function deleteItem(item) {
-    let id = productIds[item]
-    console.log(id)
-    let x = items
-    const index = x.indexOf(item)
+    let id = productIds[item];
+    console.log(id);
+    let x = items;
+    const index = x.indexOf(item);
     if (index > -1) {
-      x.splice(index, 1)
+      x.splice(index, 1);
     }
-    
-    setItems(x)
-    console.log(x)
+
+    setItems(x);
+    console.log(x);
     await API.graphql({
       query: deleteProductos,
       variables: {
-        input: {id}
-      }
-    })
-    console.log()
-    updateOrder()
-    
+        input: { id },
+      },
+    });
+    console.log();
+    updateOrder();
   }
 
   return (
@@ -156,58 +195,58 @@ const Lista = () => {
           value={entry}
           onChange={(el) => setEntry(el.target.value)}
         />
-        <Button onClick={createItem}  className="mx-3" color="success">
+        <Button onClick={createItem} className="mx-3" color="success">
           +
         </Button>
       </div>
-      <Button className="mt-4" onClick={updateOrder}>Save</Button>
+      <Button className="mt-4" onClick={updateOrder}>
+        Save
+      </Button>
       <div style={{ height: "100px" }}></div>
-      <Grid
-        container
-        direction="row"
-        justifyContent="space-evenly"
-        alignItems="row"
-        rowSpacing={1}
-        className="grid"
-        columnSpacing={{ xs: 1, sm: 2, md: 3 }}
-        paddingBottom="8px"
-      >
-        <Grid item xs={4}>
-          <div style={{ textAlign: "center" }}>
-            {store !== "Costco" && (
-              <Button onClick={() => setStore("Costco")}>Costco</Button>
-            )}
-            {store === "Costco" && (
-              <Button style={{ backgroundColor: "grey" }}>Costco</Button>
-            )}
-          </div>
-        </Grid>
-        <Grid item xs={4}>
-          <div style={{ textAlign: "center" }}>
-            {store !== "Super" && (
-              <Button onClick={() => setStore("Super")}>Super</Button>
-            )}
-            {store === "Super" && (
-              <Button style={{ backgroundColor: "grey" }}>Super</Button>
-            )}
-          </div>
-        </Grid>
-        <Grid item xs={4}>
-          <div style={{ textAlign: "center" }}>
-            {store !== "Otro" && (
-              <Button onClick={() => setStore("Otro")}>Otro</Button>
-            )}
-            {store === "Otro" && (
-              <Button style={{ backgroundColor: "grey" }}>Otro</Button>
-            )}
-          </div>
-        </Grid>
-      </Grid>
+      <AnimatePresence>
+        {scrollDir != "down" && (
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            style={{display:'flex', width:'100%', flexDirection:"row", justifyContent:'space-around'}}
+            className="stores"
+          >
+            <Grid item xs={4}>
+              <div style={{ textAlign: "center" }}>
+                {store !== "Costco" && (
+                  <Button onClick={() => setStore("Costco")}>Costco</Button>
+                )}
+                {store === "Costco" && (
+                  <Button style={{ backgroundColor: "grey" }}>Costco</Button>
+                )}
+              </div>
+            </Grid>
+            <Grid item xs={4}>
+              <div style={{ textAlign: "center" }}>
+                {store !== "Super" && (
+                  <Button onClick={() => setStore("Super")}>Super</Button>
+                )}
+                {store === "Super" && (
+                  <Button style={{ backgroundColor: "grey" }}>Super</Button>
+                )}
+              </div>
+            </Grid>
+            <Grid item xs={4}>
+              <div style={{ textAlign: "center" }}>
+                {store !== "Otro" && (
+                  <Button onClick={() => setStore("Otro")}>Otro</Button>
+                )}
+                {store === "Otro" && (
+                  <Button style={{ backgroundColor: "grey" }}>Otro</Button>
+                )}
+              </div>
+            </Grid>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </div>
   );
 };
 
 export default Lista;
-
-
-
